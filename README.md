ec8c6cb378f1df4a98a7cec80983c0fb02c04f12
Andrei Horbach <a.horbach@studionx.ru>
2021-11-16 01:25:01 +0300

    chore: add python

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is it?
+[Web service](https://en.wikipedia.org/wiki/Web_service) service in the www (world wide web) that listening for HTTP requests and can answer you with some format (HTML, JSON).
---
+++ python/main.py
@@ -0,0 +1,29 @@
+"""
+Fast API application
+ref: https://fastapi.tiangolo.com/
+"""
+from os import environ
+from socket import gethostname
+from datetime import datetime
+from uuid import uuid4
+from fastapi import FastAPI, status
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main_payload() -> dict:
+    """Check container"""
+    return {"language": "python","hostname": gethostname(), "timestamp": datetime.now(), "uuid": uuid4()}
+
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def check_health() -> dict:
+    """Smoke test ep"""
+    return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    port = int(environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
