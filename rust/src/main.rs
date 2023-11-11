1954f2373ad5cd32680ece65557e1e479efa8ff1
Andrew Horbach <a.horbach@nexters.com>
2023-11-11 13:29:08 +0200

    add rust ht

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,14 @@ use serde::Serialize;
 use warp::{reply::json, Filter, Rejection, Reply};
 use gethostname::gethostname;
 use uuid::Uuid;
+use std::env;
 
+fn get_env_as_int(key: &str, default: u16) -> u16 {
+    match env::var(key) {
+        Ok(value) => value.parse().unwrap_or(default),
+        Err(_) => default,
+    }
+}
 
 type WebResult<T> = std::result::Result<T, Rejection>;
 
@@ -15,6 +22,12 @@ pub struct PayloadResponse {
     uuid: String,
 }
 
+#[derive(Serialize)]
+struct Health {
+    status: String,
+}
+
+
 pub async fn payload_handler() -> WebResult<impl Reply> {
     let response_json = &PayloadResponse {
         language: "rust".to_string(),
@@ -25,20 +38,31 @@ pub async fn payload_handler() -> WebResult<impl Reply> {
     Ok(json(response_json))
 }
 
+pub async fn health_handler() -> WebResult<impl Reply> {
+    let response_json = &Health {
+        status: "ok".to_string(),
+    };
+    Ok(json(response_json))
+}
+
+
 #[tokio::main]
 async fn main() {
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "api=info");
-    }
+    env::set_var("RUST_LOG", "api=info");
     pretty_env_logger::init();
 
-    // let health_checker = warp::path!("api" / "healthchecker")
-    let payload = warp::path!("api" / "root")
+    let port = get_env_as_int("PORT", 8095);
+
+    let payload = warp::path!("api")
         .and(warp::get())
         .and_then(payload_handler);
 
-    let routes = payload.with(warp::log("api"));
+    let health = warp::path!("health")
+        .and(warp::get())
+        .and_then(health_handler);
+
+    let routes =  warp::get().and(payload.or(health)).with(warp::log("api"));
 
     println!("🚀 Server started successfully");
-    warp::serve(routes).run(([0, 0, 0, 0], 8095)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
